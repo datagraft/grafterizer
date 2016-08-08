67dc140d397b0a94692aafc60f7b37ee3faaa470
nivemaham <nivemaham@gmail.com>
2016-08-08 14:33:29 +0200

    convert excel to csv from client side; minimal poc to choose first sheet by default. supports csv, excel sheets

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -46,6 +46,7 @@ module.exports = function(config) {
       'bower_components/raven-js/dist/raven.js',
       'bower_components/angular-mocks/angular-mocks.js',
       'bower_components/oi.select/dist/select-tpls.min.js',
+      'bower_components/js-xlsx/dist/xlsx.js',
       // endbower
       'app/scripts/**/*.js',
       'test/mock/**/*.js',
